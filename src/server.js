c3ace2a92846378a9cc7bfe95c69d873134429f4
zdy <zdy07015394@gmail.com>
2018-10-25 21:07:02 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const fs = require('fs')
+const { resolve } = require('path')
+const hbs = require('hbs')
+
+const app = express()
+
+// 处理partial
+hbs.registerPartials(resolve(__dirname, '../views/partials'))
+
+hbs.registerHelper('getYear', () => new Date().getFullYear())
+hbs.registerHelper('upperCase', (text) => {
+  return text.toUpperCase()
+})
+
+app.set('view engine', 'hbs')
+
+// 处理静态文件
+app.use(express.static(resolve(__dirname, '../public')))
+
+// 中间件
+app.use((req, res, next) => {
+  let now = new Date().toString()
+  let log = `${now} ${req.method} ${req.url}`
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('unable to append log')
+    }
+  })
+  next()
+})
+
+app.use((req, res, next) => {
+  res.render(resolve(__dirname, '../views/err.hbs'))
+})
+
+
+
+app.get('/', (req, res) => {
+  // res.send('<h1>hello world</h1>')
+
+  //send json
+  res.send({
+    name: 'hhh',
+    age: 'inifity'
+  })
+
+})
+
+app.get('/test1', (req, res) => {
+  res.render(resolve(__dirname, '../views/test.hbs'), {
+    title: 'test'
+  })
+})
+
+app.listen(3000, () => console.log('server start at port 3000'))
